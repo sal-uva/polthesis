4c49e171a965b37ad41783df7e96ac30d3223703
Sal Hagen <hagen.sal@gmail.com>
2018-03-13 18:52:42 +0100

    finished colocation functions, ready for RankFlow graphs

--- getNgrams.py
+++ getNgrams.py
@@ -17,8 +17,8 @@ from nltk.corpus import stopwords
 # media_hash, media_orig, spoiler, deleted, capcode, email, name, trip,
 # title, comment, sticky, locked, poster_hash, poster_country, exif
 
-def getNgrams(querystring, fullcomment = True, colocationamount = 1, windowsize = 4, outputlimit = 10, separateontime = False, timeseparator = 'days'):
-
+def getNgrams(querystring, fullcomment = True, colocationamount = 1, windowsize = 4, outputlimit = 10, separateontime = False, timeseparator = 'days', frequencyfilter = 1):
+	separateontime = separateontime
 	maxoutput = outputlimit
 
 	print('Connecting to database')
@@ -67,42 +67,52 @@ def getNgrams(querystring, fullcomment = True, colocationamount = 1, windowsize 
 						tokens.append(word)
 
 		print('Generating colocations')
-		if colocationamount == 1:
-			finder = BigramCollocationFinder.from_words(tokens, window_size=windowsize)
-		if colocationamount == 2:
-			finder = TrigramCollocationFinder.from_words(tokens, window_size=windowsize)
-
-		fullcolocations = sorted(finder.ngram_fd.items(), key=operator.itemgetter(1), reverse=True)[0:outputlimit]
+		colocations = calculateColocation(tokens, windowsize, colocationamount, querystring, fullcomment, frequencyfilter, outputlimit)
+		str_colocations = str(colocations)
 
 	elif separateontime == True:
 		di_timestrings = {}
-		str_timedivided = ''
+		str_timestring = ''
 		str_timeinterval = ''
-		currenttimeinverval = ''
-		
+
+		if timeseparator == 'days':
+			dateformat = '%Y-%m-%d'
+		elif timeseparator == 'months':
+			dateformat = '%Y-%m'
+		currenttimeinverval = datetime.fromtimestamp(li_timestamplist[0]).strftime(dateformat)
+
+		print('starting with ' + currenttimeinverval)
 		for index, timestamp in enumerate(li_timestamplist):
 			#check if the post is the same date as the previous post
-			if currenttimeinverval == datetime.fromtimestamp(li_timestamplist[index]).strftime("%Y-%m-%d"):
-				str_timedivided = str(str_timedivided) + str(li_posts[index]) + ' '
+			if currenttimeinverval == datetime.fromtimestamp(timestamp).strftime(dateformat):
+				str_timestring = str(str_timestring) + str(li_posts[index]) + ' '
 			#if its a new day/month, make a new dict entry
 			else:
-				di_timestrings[str_timeinterval] = str_timedivided
-				str_timeinterval = datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d")
-				str_timedivided = str(li_posts[index])
+				#store old string
+				di_timestrings[currenttimeinverval] = str_timestring
+				str_timeinterval = datetime.fromtimestamp(timestamp).strftime(dateformat)
+				str_timestring = str(li_posts[index])
 				currenttimeinverval = str_timeinterval
-		#print(di_timestrings)
-		
+				print('new timeframe: ' + str(currenttimeinverval))
+			di_timestrings[currenttimeinverval] = str_timestring
 		di_time_ngrams = {}
+
+		#if there's no matches with the querystring
+		if len(di_timestrings) < 1:
+			return('No matches')
+
 		for key, value in di_timestrings.items():
+			print('starting colocations for ' + str(key))
+
 			#all text to lowercase
 			longstring = value.lower()
-
+			#print(longstring)
+			
 			regex = re.compile("[^a-zA-Z]")		#excludes numbers, might have to revise this
-			longstring = regex.sub(" ",longstring)
+			longstring = regex.sub(" ", longstring)
 
 			tokenizer = RegexpTokenizer(r"\w+")
 			tmptokens = tokenizer.tokenize(longstring)
-
 			tokens = []
 
 			print('Creating filtered tokens (i.e. excluding stopwords and forbiddenwords)')
@@ -112,24 +122,81 @@ def getNgrams(querystring, fullcomment = True, colocationamount = 1, windowsize 
 						match = re.search(word, r'(\d{9})')
 						if not match:		#if it's a post number
 							tokens.append(word)
-			if colocationamount == 1:
-				finder = BigramCollocationFinder.from_words(tokens, window_size=windowsize)
-			if colocationamount == 2:
-				finder = TrigramCollocationFinder.from_words(tokens, window_size=windowsize)
-			colocations = sorted(finder.ngram_fd.items(), key=operator.itemgetter(1), reverse=True)[0:outputlimit]
-			di_time_ngrams[key] = colocations
-			print(colocations)
 
-		fullcolocations = str(di_time_ngrams)
+			di_time_ngrams[key] = calculateColocation(tokens, windowsize, colocationamount, querystring, fullcomment, frequencyfilter, outputlimit)
+
+			#bigram_measures = nltk.collocations.BigramAssocMeasures()
+			#raw_freq_ranking = finder.nbest(bigram_measures.raw_freq, 10) #top-10
+		colocations = di_time_ngrams
+		str_colocations = str(di_time_ngrams)
+
+	print('Generating RankFlow-capable csv of colocations')
+	rankflow_df = createColocationCsv(colocations)
+	rankflow_df.to_csv('colocations/' + querystring + '-colocations.csv', encoding='utf-8')
 
 	print('Writing restults to textfile')
 	write_handle = open('colocations/' + str(querystring) + '-colocations.txt',"w")
-	write_handle.write(str(fullcolocations))
+	write_handle.write(str(str_colocations))
 	write_handle.close()
+
+	return(colocations)
+
+def calculateColocation(inputtokens, windowsize, colocationamount, querystring, fullcomment, frequencyfilter, outputlimit):
+	#guide here http://www.nltk.org/howto/collocations.html
+	#generate bigrams
+	if colocationamount == 1:
+		finder = BigramCollocationFinder.from_words(inputtokens, window_size=windowsize)
+		#filter on bigrams that only contain the query string
+		if fullcomment == False:
+			word_filter = lambda w1, w2: querystring not in (w1, w2)
+			finder.apply_ngram_filter(word_filter)
+	#generate trigrams
+	if colocationamount == 2:
+		finder = TrigramCollocationFinder.from_words(inputtokens, window_size=windowsize)
+		#filter on trigrams that only contain the query string
+		if fullcomment == False:
+			word_filter = lambda w1, w2, w3: querystring not in (w1, w2, w3)
+			finder.apply_ngram_filter(word_filter)
+	finder.apply_freq_filter(frequencyfilter)
+
+	colocations = sorted(finder.ngram_fd.items(), key=operator.itemgetter(1), reverse=True)[0:outputlimit]
+	return colocations
+
+def createColocationCsv(inputcolocations):
 	
-	print(fullcolocations)
-	return(fullcolocations)
+	columns = []
+	df = pd.DataFrame()
 
-getNgrams('nigger', fullcomment=True, colocationamount=2, windowsize=4, outputlimit=3, separateontime=True)
+	for key, values in inputcolocations.items():
+		li_colocations = []
+		li_mentions = []
+		mentions = 0
+		columns.append(key)
+		columns.append('mentions')
 
-print('finished')
+		for colocation_tuple in values:
+			str_colocations = ''
+			#loop through tuple with colocation words and frequency (at the end)
+			for index, tuple_value in enumerate(colocation_tuple):
+				
+				if type(tuple_value) is tuple:
+					for string in tuple_value:
+						str_colocations = str_colocations + ' ' + string
+				else:
+					mentions = tuple_value
+
+			li_colocations.append(str_colocations)
+			li_mentions.append(mentions)
+
+		tmp_df = pd.DataFrame()
+		tmp_df[key] = li_colocations
+		tmp_df['mentions'] = li_mentions
+		df = pd.concat([df, tmp_df], axis=1)
+		
+	print(df)
+	return(df)
+
+ngrams = getNgrams('clinton', fullcomment=True, colocationamount=1, windowsize=3, frequencyfilter=0, outputlimit=10, separateontime=True, timeseparator='days')
+
+print(ngrams)
+print('Colocations completed')
