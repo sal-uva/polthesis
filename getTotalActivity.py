5079df73609ea9e2bae211fdc6ba5fb1478de27b
DESKTOP\hagen <hagen.sal@gmail.com>
2018-03-26 15:53:09 +0200

    Total activity and began working on normalised frequencies

--- getTotalActivity.py
+++ getTotalActivity.py
@@ -18,21 +18,25 @@ from nltk.corpus import stopwords
 
 def getTotalActivity(dateformat='months'):
 	print('Connecting to database')
-	conn = sqlite3.connect("../4plebs_pol_test_database.db")
+	conn = sqlite3.connect("../4plebs_pol_18_03_2018.db")
 
 	if dateformat == 'months':
 		dateformat = '%Y-%m'
 	elif dateformat == 'days':
 		dateformat = '%Y-%m-%d'
-	print('Beginning SQL query')
-	dates = pd.read_sql_query("SELECT DISTINCT(timestamp) FROM poldatabase;", conn)
-	print(dates)
-	li_alldates = dates['timestamp'].values.tolist()
-	li_alldates.sort()
-	firstdate = li_alldates[0]
-	lastdate = li_alldates[len(li_alldates) - 1]
-	print(firstdate, lastdate)
 
+	print('Beginning SQL query')
+
+	li_dates = []
+
+	# dates = pd.read_sql_query("SELECT MIN(timestamp)mintimestamp, MAX(timestamp)maxtimestamp FROM poldatabase_18_03_2018;", conn)
+	# print(dates)
+	firstdate = 1378739071
+	lastdate = 1521370386
+	# firstdate = dates['mintimestamp'][0]
+	# lastdate = dates['maxtimestamp'][0]
+	print(firstdate, lastdate)
+	
 	li_dates = []
 	li_countposts = []
 
@@ -41,7 +45,9 @@ def getTotalActivity(dateformat='months'):
 	newtime = ''
 	minquerydate = firstdate
 	currenttime = datetime.fromtimestamp(minquerydate).strftime(dateformat)
-	for timestamp in li_alldates:
+	timestamp = firstdate
+	while timestamp < lastdate:
+		timestamp = timestamp + 1
 		#print(timestamp)
 		if timestamp != lastdate:
 			newtime = datetime.fromtimestamp(timestamp).strftime(dateformat)
@@ -50,12 +56,14 @@ def getTotalActivity(dateformat='months'):
 				print('SQL query for ' + str(newtime))
 				timestring = str(newtime)
 				maxquerydate = timestamp
-				df = pd.DataFrame
-				df = pd.read_sql_query("SELECT COUNT(*)count FROM poldatabase WHERE timestamp > :firsttime AND timestamp < :lasttime;", conn, params={'dateheader': newtime, 'firsttime': minquerydate, 'lasttime': maxquerydate})
+				print(minquerydate, maxquerydate)
 				
+				df = pd.read_sql_query("SELECT COUNT(*)count FROM poldatabase_18_03_2018 WHERE (timestamp BETWEEN ? AND ?);", conn, params=[minquerydate, maxquerydate])
+				print(df)
+				print(df['count'])
 				li_dates.append(str(newtime))
 				li_countposts.append(df['count'][0])
-				
+				print(li_countposts)
 				minquerydate = timestamp
 				currenttime = newtime
 
@@ -65,4 +73,4 @@ def getTotalActivity(dateformat='months'):
 	df_timethreads.to_csv('all_activity.csv', index=False)
 	print(df_timethreads)
 
-getTotalActivity(dateformat='days')
+getTotalActivity(dateformat='months')
--- substringFilter.py
+++ substringFilter.py
@@ -11,7 +11,62 @@ from collections import OrderedDict
 # media_hash, media_orig, spoiler, deleted, capcode, email, name, trip,
 # title, comment, sticky, locked, poster_hash, poster_country, exif
 
-def substringFilter(inputstring, histogram = False, inputtime = 'months'):
+#all comments:
+# 2013-12	61519
+# 2014-01	1212183
+# 2014-02	1169314
+# 2014-03	1057428
+# 2014-04	1234152
+# 2014-05	1135162
+# 2014-06	1195313
+# 2014-07	1127383
+# 2014-08	1491844
+# 2014-09	1738433
+# 2014-10	1571584
+# 2014-11	1424278
+# 2014-12	1441101
+# 2015-01	909278
+# 2015-02	772111
+# 2015-03	890495
+# 2015-04	1197976
+# 2015-05	1300518
+# 2015-06	1381517
+# 2015-07	1392446
+# 2015-08	1597274
+# 2015-09	1903111
+# 2015-10	2000023
+# 2015-11	2004026
+# 2015-12	2344421
+# 2016-01	2592275
+# 2016-02	2925369
+# 2016-03	3111713
+# 2016-04	3736528
+# 2016-05	3048962
+# 2016-06	3131789
+# 2016-07	3642871
+# 2016-08	4314923
+# 2016-09	3618363
+# 2016-10	3759066
+# 2016-11	4418571
+# 2016-12	5515200
+# 2017-01	4187400
+# 2017-02	5191531
+# 2017-03	4368911
+# 2017-04	4386181
+# 2017-05	4428757
+# 2017-06	4374011
+# 2017-07	4020058
+# 2017-08	3752418
+# 2017-09	4087688
+# 2017-10	3703119
+# 2017-11	3931560
+# 2017-12	4122068
+# 2018-01	3584861
+# 2018-02	3624546
+# 2018-03	3468642
+
+
+def substringFilter(inputstring, histogram = False, inputtime = 'months', normalised=False):
 	querystring = inputstring.lower()
 	print('Connecting to database')
 	conn = sqlite3.connect("../4plebs_pol_18_03_2018.db")
@@ -22,9 +77,9 @@ def substringFilter(inputstring, histogram = False, inputtime = 'months'):
 	df.to_csv('test_output.csv')
 
 	if histogram == True:
-		createHistogram(df, querystring, inputtime)
+		createHistogram(df, querystring, inputtime, normalised)
 
-def createHistogram(inputdf, querystring, inputtimeformat):
+def createHistogram(inputdf, querystring, inputtimeformat, normalised):
 	df = inputdf
 	timeformat = inputtimeformat
 	li_timestamps = df['timestamp'].values.tolist()
@@ -63,13 +118,44 @@ def createHistogram(inputdf, querystring, inputtimeformat):
 		print(li_timeticks)
 		dateformat = '%m-%y'
 		mpl_dates = matplotlib.dates.epoch2num(li_timestamps)
+		
 		timebuckets = [datetime.strptime(i, "%m-%y") for i in li_timeticks]
 		timebuckets = matplotlib.dates.date2num(timebuckets)
-		print(type(timebuckets[1]))
 
-	# plot it
+		print(li_timeticks)
+		count_timestamps = 0
+		month = 0
+		mpl_normaldates = []
+		if normalised:
+			di_totalcomment = {'2013-11': 0, '2013-12': 61519, '2014-01': 1212183, '2014-02': 1169314, '2014-03': 1057428, '2014-04': 1234152, '2014-05': 1135162, '2014-06': 1195313, '2014-07': 1127383, '2014-08': 1491844, '2014-09': 1738433, '2014-10': 1571584, '2014-11': 1424278, '2014-12': 1441101, '2015-01': 909278, '2015-02': 772111, '2015-03': 890495, '2015-04': 1197976, '2015-05': 1300518, '2015-06': 1381517, '2015-07': 1392446, '2015-08': 1597274, '2015-09': 1903111, '2015-10': 2000023, '2015-11': 2004026, '2015-12': 2344421, '2016-01': 2592275, '2016-02': 2925369, '2016-03': 3111713, '2016-04': 3736528, '2016-05': 3048962, '2016-06': 3131789, '2016-07': 3642871, '2016-08': 4314923, '2016-09': 3618363, '2016-10': 3759066, '2016-11': 4418571, '2016-12': 5515200, '2017-01': 4187400, '2017-02': 5191531, '2017-03': 4368911, '2017-04': 4386181, '2017-05': 4428757, '2017-06': 4374011, '2017-07': 4020058, '2017-08': 3752418, '2017-09': 4087688, '2017-10': 3703119, '2017-11': 3931560, '2017-12': 4122068, '2018-01': 3584861, '2018-02': 3624546, '2018-03': 3468642}
+
+			li_totalcomments = [0,61519,1212183,1169314,1057428,1234152,1135162,1195313,1127383,1491844,1738433,1571584,1424278,1441101,909278,772111,890495,1197976,1300518,1381517,1392446,1597274,1903111,2000023,2004026,2344421,2592275,2925369,3111713,3736528,3048962,3131789,3642871,4314923,3618363,3759066,4418571,5515200,4187400,5191531,4368911,4386181,4428757,4374011,4020058,3752418,4087688,3703119,3931560,4122068,3584861,3624546,3468642]
+
+			for timestamp in mpl_dates:
+				if(str(datetime.fromtimestamp(timestamp).strftime("%b-%Y"))) == li_timeticks[month]:
+					count_timestamps = count_timestamps + 1
+				else:
+					normalised_mentions = (count_timestamps / (li_totalcomments[month] + 1)) * 100
+					mpl_normaldates.append(normalised_mentions)
+					month = month + 1
+			mpl_dates = mpl_normaldates
+		print(type(timebuckets[1]))
+		print(mpl_dates)
+		print(timebuckets)
+
+	# plot it!
 	fig, ax = plt.subplots(1,1)
 	ax.hist(mpl_dates, bins=timebuckets, align="left", color='red', ec="k")
+	histo = ax.hist(mpl_dates, bins=timebuckets, align="left", color='red', ec="k")
+	print(histo)
+	print(histo[0])
+	li_counts= []
+	for counts in histo[0]:
+		li_counts.append(counts)
+	print('Writing results to textfile')
+	write_handle = open('deusvultcounts.txt',"w")
+	write_handle.write(str(li_counts))
+	write_handle.close()
 
 	if timeformat == 'days':
 		ax.xaxis.set_major_locator(matplotlib.dates.DayLocator())
@@ -89,6 +175,6 @@ def createHistogram(inputdf, querystring, inputtimeformat):
 	plt.gcf().autofmt_xdate()
 	plt.show()
 
-result = substringFilter('race realis', histogram = True, inputtime='months')	#returns tuple with df and input string
+result = substringFilter('mary poppins', histogram = True, inputtime='months', normalised=True)	#returns tuple with df and input string
 
 print('finished')
