dade6eaf4a05be7d6932eb25492684206759a1f9
DESKTOP\hagen <hagen.sal@gmail.com>
2018-04-04 12:11:23 +0200

    minor adjustments

--- substringFilter.py
+++ substringFilter.py
@@ -3,6 +3,8 @@ import sqlite3
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import time
+from scipy.interpolate import spline
 from datetime import datetime, timedelta
 from collections import OrderedDict
 
@@ -19,8 +21,11 @@ def substringFilter(inputstring, histogram = False, inputtime = 'months', normal
 	print('Beginning SQL query for "' + querystring + '"')
 	df = pd.read_sql_query("SELECT timestamp, comment FROM poldatabase_18_03_2018 WHERE lower(comment) LIKE ?;", conn, params=['%' + querystring + '%'])
 	print('Writing results to csv')
-	df.to_csv('test_output.csv')
+	df.to_csv('substring_mentions/mentions_' + querystring + '.csv')
 
+	# FOR DEBUGGING PURPOSES:
+	#df = pd.read_csv('substring_mentions/mentions_alt-left.csv')
+	
 	if histogram == True:
 		createHistogram(df, querystring, inputtime, normalised)
 
@@ -64,35 +69,18 @@ def createHistogram(inputdf, querystring, inputtimeformat, normalised):
 		dateformat = '%m-%y'
 		mpl_dates = matplotlib.dates.epoch2num(li_timestamps)
 		
+		
 		timebuckets = [datetime.strptime(i, "%m-%y") for i in li_timeticks]
 		timebuckets = matplotlib.dates.date2num(timebuckets)
 
 		print(li_timeticks)
 		count_timestamps = 0
 		month = 0
-		mpl_normaldates = []
 		if normalised:
-			di_totalcomment = {'2013-11': 1, '2013-12': 61519, '2014-01': 1212183, '2014-02': 1169314, '2014-03': 1057428, '2014-04': 1234152, '2014-05': 1135162, '2014-06': 1195313, '2014-07': 1127383, '2014-08': 1491844, '2014-09': 1738433, '2014-10': 1571584, '2014-11': 1424278, '2014-12': 1441101, '2015-01': 909278, '2015-02': 772111, '2015-03': 890495, '2015-04': 1197976, '2015-05': 1300518, '2015-06': 1381517, '2015-07': 1392446, '2015-08': 1597274, '2015-09': 1903111, '2015-10': 2000023, '2015-11': 2004026, '2015-12': 2344421, '2016-01': 2592275, '2016-02': 2925369, '2016-03': 3111713, '2016-04': 3736528, '2016-05': 3048962, '2016-06': 3131789, '2016-07': 3642871, '2016-08': 4314923, '2016-09': 3618363, '2016-10': 3759066, '2016-11': 4418571, '2016-12': 5515200, '2017-01': 4187400, '2017-02': 5191531, '2017-03': 4368911, '2017-04': 4386181, '2017-05': 4428757, '2017-06': 4374011, '2017-07': 4020058, '2017-08': 3752418, '2017-09': 4087688, '2017-10': 3703119, '2017-11': 3931560, '2017-12': 4122068, '2018-01': 3584861, '2018-02': 3624546, '2018-03': 3468642}
+			di_totalcomment = {'11-13': 1, '12-13': 61519, '01-14': 1212183, '02-14': 1169314, '03-14': 1057428, '04-14': 1234152, '05-14': 1135162, '06-14': 1195313, '07-14': 1127383, '08-14': 1491844, '09-14': 1738433, '10-14': 1571584, '11-14': 1424278, '12-14': 1441101, '01-15': 909278, '02-15': 772111, '03-15': 890495, '04-15': 1197976, '05-15': 1300518, '06-15': 1381517, '07-15': 1392446, '08-15': 1597274, '09-15': 1903111, '10-15': 23000, '11-15': 26004, '12-15': 2344421, '01-16': 2592275, '02-16': 2925369, '03-16': 3111713, '04-16': 3736528, '05-16': 3048962, '06-16': 3131789, '07-16': 3642871, '08-16': 4314923, '09-16': 3618363, '10-16': 3759066, '11-16': 4418571, '12-16': 5515200, '01-17': 4187400, '02-17': 5191531, '03-17': 4368911, '04-17': 4386181, '05-17': 4428757, '06-17': 4374011, '07-17': 4020058, '08-17': 3752418, '09-17': 4087688, '10-17': 3703119, '11-17': 3931560, '12-17': 4122068,'01-18': 3584861, '02-18': 3624546, '03-18': 3468642}
 
 			li_totalcomments = [1,61519,1212183,1169314,1057428,1234152,1135162,1195313,1127383,1491844,1738433,1571584,1424278,1441101,909278,772111,890495,1197976,1300518,1381517,1392446,1597274,1903111,2000023,2004026,2344421,2592275,2925369,3111713,3736528,3048962,3131789,3642871,4314923,3618363,3759066,4418571,5515200,4187400,5191531,4368911,4386181,4428757,4374011,4020058,3752418,4087688,3703119,3931560,4122068,3584861,3624546,3468642]
 
-			# li_timestamps = sorted(li_timestamps)
-			# for timestamp in li_timestamps:
-			# 	print('starting ' + str(li_timeticks[month]))
-			# 	print(str(datetime.fromtimestamp(timestamp).strftime("%m")))
-			# 	if(str(datetime.fromtimestamp(timestamp).strftime("%m"))) == li_timeticks[month][:2]:
-			# 		count_timestamps = count_timestamps + 1
-			# 	else:
-			# 		normalised_mentions = (count_timestamps / (li_totalcomments[month])) * 100
-			# 		print(normalised_mentions)
-			# 		print('new month')
-			# 		mpl_normaldates.append(normalised_mentions)
-			# 		month = month + 1
-			# normalised_mentions = (count_timestamps / (li_totalcomments[month])) * 100
-			# print(normalised_mentions)
-			# print('new month')
-			# mpl_normaldates.append(normalised_mentions)
-			#mpl_dates = mpl_normaldates
 		print(mpl_dates)
 		print(timebuckets)
 
@@ -100,27 +88,6 @@ def createHistogram(inputdf, querystring, inputtimeformat, normalised):
 	fig, ax = plt.subplots(1,1)
 	ax.hist(mpl_dates, bins=timebuckets, align="left", color='red', ec="k")
 	histo = ax.hist(mpl_dates, bins=timebuckets, align="left", color='red', ec="k")
-	
-	li_counts = [0]
-	for counts in histo[0]:
-		li_counts.append(counts)
-	print(len(li_timeticks))
-	newli_timeticks = list(di_totalcomment.keys())
-	print(len(li_counts))
-	li_normalisedcounts = []
-
-	for index, count in enumerate(li_counts):
-		count_normalised = (count / li_totalcomments[index]) * 100
-		print(count_normalised)
-		li_normalisedcounts.append(count_normalised)
-
-	print(len(li_normalisedcounts))
-	print('Writing results to csv')
-	finaldf = pd.DataFrame(columns=['date','count','percentage'])
-	finaldf['date'] = li_timeticks
-	finaldf['percentage'] = li_normalisedcounts
-	finaldf['count'] = li_counts
-	finaldf.to_csv('substring_mentions/occurrances_' + querystring + '.csv')
 
 	if timeformat == 'days':
 		ax.xaxis.set_major_locator(matplotlib.dates.DayLocator())
@@ -138,12 +105,121 @@ def createHistogram(inputdf, querystring, inputtimeformat, normalised):
 
 	#rotate labels
 	plt.gcf().autofmt_xdate()
-	plt.savefig('../visualisations/' + querystring + '_full.svg')
-	#plt.show()
+	plt.savefig('../visualisationsold/' + querystring + '_full.svg')
+	# plt.show(block=False)
+	# time.sleep(2)
+	plt.close()
 
-li_querywords = ['blacks','niggers','cuck','whites','gook','thradthot','we must','ethnic','trump','god emperor','stump the trump','bust the trump','feminists','jews']
+	newli_timeticks = list(di_totalcomment.keys())
+	print(len(newli_timeticks))
+
+	li_counts = []
+	print(ax.xaxis.get_ticklabels())
+	li_axisticks = ax.xaxis.get_majorticklabels()
+	li_axisticks = li_axisticks[:-3]
+	li_axisticks = li_axisticks[3:]
+	print(li_axisticks)
+	li_matchticks = []
+	for text in li_axisticks:
+		strtext = text.get_text()
+		li_matchticks.append(strtext)
+	print(len(li_matchticks))
+	print('matching months: ' + str(li_matchticks))
+	print('histo data:')
+	print(histo)
+	print('histo len: ' + str(len(histo[0])))
+	print(len(li_matchticks))
+	print(li_matchticks)
+	print(newli_timeticks)
+	print(histo[0])
+	#loop over each month
+	histoindex = 0
+	for month in range(53):
+		occurance = False
+		for registeredmonth in li_matchticks:
+			#print(registeredmonth)
+			if registeredmonth == newli_timeticks[month]:
+				print('Month ' + str(histoindex) + ' found')
+				li_counts.append(histo[0][histoindex])
+				histoindex = histoindex + 1
+				occurance = True
+		if occurance == False:						#if the string did not occur, write 0
+			print('no occurances this month')
+			li_counts.append(0)
+	
+	print('li_counts length: ' + str(len(li_counts)))
+
+	li_normalisedcounts = []
+	for index, count in enumerate(li_counts):
+		print(li_totalcomments[index])
+		count_normalised = (count / li_totalcomments[index]) * 100
+		print(count_normalised)
+		li_normalisedcounts.append(count_normalised)
+
+	li_datesformatted = []
+	li_histodates = []
+	for date in newli_timeticks:
+		dateobject = datetime.strptime(date, '%m-%y')
+		formatteddate = datetime.strftime(dateobject, '%Y-%b')
+		histodate = datetime.strftime(dateobject, '%b %y')
+		li_datesformatted.append(formatteddate)
+		li_histodates.append(histodate)
+
+	print('Writing results to csv')
+
+	# only keep months that have full data
+	del newli_timeticks[0]
+	del li_datesformatted[0]
+	del li_histodates[0]
+	del li_normalisedcounts[0]
+	del li_counts[0]
+	del newli_timeticks[len(newli_timeticks) - 1]
+	del li_histodates[len(li_histodates) - 1]
+	del li_normalisedcounts[len(li_normalisedcounts) - 1]
+	del li_counts[len(li_counts) - 1]
+	del li_datesformatted[len(li_datesformatted) - 1]
+
+	print(li_datesformatted)
+	print(len(newli_timeticks))
+	print(len(li_datesformatted))
+	print(len(li_normalisedcounts))
+	print(len(li_counts))
+
+	finaldf = pd.DataFrame(columns=['date','dateformatted','count','percentage'])
+	finaldf['date'] = newli_timeticks
+	finaldf['dateformatted'] = li_datesformatted
+	finaldf['percentage'] = li_normalisedcounts
+	finaldf['count'] = li_counts
+	finaldf.to_csv('substring_mentions/occurrances_' + querystring + '.csv', index=False)
+
+	df2 = pd.DataFrame(index=li_histodates[1:], columns=['count','percentage'])
+	df2['dates'] = li_histodates[1:]
+	df2['count'] = li_counts[1:]
+	df2['percentage'] = li_normalisedcounts[1:]
+	plotNewGraph(df2, querystring)
+
+def plotNewGraph(df, query):
+
+	fig = plt.figure(figsize=(12, 8))
+	fig.set_dpi(100)
+	ax1 = fig.add_subplot(111)
+	ax2 = ax1.twinx()
+
+	df.plot(ax=ax1, y='count', kind='bar', legend=False, width=.9, color='#52b6dd');
+	df.plot(ax=ax2, y='percentage', legend=False, kind='line', linewidth=2, color='#d12d04');
+	ax1.set_axisbelow(True)
+	ax1.set_xticklabels(df['dates'])
+	ax1.grid(color='#e5e5e5',linestyle='dashed', linewidth=.6)
+	ax1.set_ylabel('Absolute amount', color='#52b6dd')
+	ax2.set_ylabel('Percentage of total comments', color='#d12d04')
+	ax2.set_ylim(bottom=0)
+	plt.title('Amount of 4chan/pol/ comments containing "' + query + '"')
+
+	plt.savefig('../visualisations/substring_counts/' + query + '.svg', dpi='figure')
+	plt.savefig('../visualisations/substring_counts/' + query + '.jpg', dpi='figure')
+
+li_querywords = ['jidf','gook','thot','paypig','orbiter','alpha male','chucklefuck','spic','coalburner']
 
 for word in li_querywords:
 	result = substringFilter(word, histogram = True, inputtime='months', normalised=True)	#returns tuple with df and input string
-
 print('finished')
