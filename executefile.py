fa4606a56cceb00b93938131eed7ca2f3daff2ec
DESKTOP\hagen <hagen.sal@gmail.com>
2018-10-12 10:28:04 +0200

    getTokens cleaning

--- executefile.py
+++ executefile.py
@@ -6,11 +6,19 @@ import sqlite3
 import pandas as pd
 import getTripleParsContent as threepars
 import getNgrams as ngram
+import getTokens
 
+li_quarterly_months = ['2016-04','2016-07','2016-10','2017-01','2017-04','2017-07','2017-10','2018-01']
 
-li_models = ['word_embedding_models/model_withparentheses_2014-01.model','word_embedding_models/model_withparentheses_2015-01.model','word_embedding_models/model_withparentheses_2016-01.model','word_embedding_models/model_withparentheses_2017-01.model','word_embedding_models/model_withparentheses_2018-01.model']
-li_months = ['2014-01','2015-01','2016-01','2017-01','2018-01']
+for index, month in enumerate(li_quarterly_months):
+	df = subs.substringFilter('all', inmonth=li_quarterly_months[index], tocsv=False)
+	li_strings = df['comment'].tolist()
+	tokens = getTokens.getTokens(li_strings, lemmatizing=True)
+	pickle.dump(tokens, open('tokens/tokens_lemma_withpars_' + month + '.p', 'wb'))
 
-df = getmod.getWordEmbeddingSimilars('(((they)))', li_models, li_months)
-print(df)
-df.to_csv('w2v_similars_(((they))).csv')
+# get models
+# li_models = ['word_embedding_models/model_withparentheses_2014-01.model','word_embedding_models/model_withparentheses_2015-01.model','word_embedding_models/model_withparentheses_2016-01.model','word_embedding_models/model_withparentheses_2017-01.model','word_embedding_models/model_withparentheses_2018-01.model']
+
+# df = getmod.getWordEmbeddingSimilars('(((they)))', li_models, li_months)
+# print(df)
+# df.to_csv('w2v_similars_(((they))).csv')
